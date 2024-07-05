ee8a7579a37804c6c960a13492a39c49a6e7d397
Travis Hinkelman <thinkelman@esassoc.com>
2024-07-05 10:47:05 -0700

    Change location of show data button

--- app.R
+++ app.R
@@ -47,16 +47,13 @@ points <- readRDS("Cul_wqpoints.rds") %>%
 # Shiny UI
 ui <- page_fillable(
   title = "Culebra WQ",
-  layout_columns(
-    h4("Culebra Water Quality Dashboard"),
-    h4(" "),
-    actionButton("DTshow", "Show Filtered Data"),
-    col_widths = c(6, 4, 2)
-  ),
+  h4("Culebra Water Quality Dashboard"),
   layout_columns(
     selectInput(inputId = 'Locsel', label = 'Location', choices = c('Nearshore', 'Watershed'), selected = 'Nearshore'),
     selectInput(inputId = 'Levelsel', label = 'Sample Level', choices = NULL),
-    selectInput(inputId = 'Parmsel', label = 'Parameter', choices = NULL)
+    selectInput(inputId = 'Parmsel', label = 'Parameter', choices = NULL),
+    actionButton("DTshow", "Show Filtered Data", class = "m-4"), # m-4 increases margins to align with other inputs in that row
+    col_widths = c(3, 3, 3, 3)
   ),
   layout_columns(
     card(full_screen = TRUE, leafletOutput('map')),
