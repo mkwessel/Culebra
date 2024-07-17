2308c9fcdbcc254c2f3abb146e3d31f1bff074e7
Travis Hinkelman <thinkelman@esassoc.com>
2024-07-17 15:55:44 -0700

    Change layout

--- ui.R
+++ ui.R
@@ -1,18 +1,23 @@
 
-page_fillable(
-  title = "Culebra WQ",
-  h4("Culebra Water Quality Dashboard"),
-  layout_columns(
-    selectInput(inputId = 'Locsel', label = 'Location', choices = c('Nearshore', 'Watershed'), selected = 'Nearshore'),
-    conditionalPanel(condition = 'input.Locsel == "Nearshore"',
-                     selectInput(inputId = 'Levelsel', label = 'Sample Level', 
+page_sidebar(
+  title = "Culebra Water Quality Dashboard",
+  window_title = "Culebra WQ",
+  sidebar = sidebar(
+    selectInput(inputId = 'environment', label = "Environment", choices = c('Nearshore', 'Watershed'), selected = 'Nearshore'),
+    conditionalPanel(condition = 'input.environment == "Nearshore"',
+                     selectInput(inputId = 'level', label = 'Sample Level', 
                                  choices = c("Surface", "Bottom", "N/A"))),
-    selectInput(inputId = 'Parmsel', label = 'Parameter', choices = NULL),
-    actionButton("DTshow", "Show Filtered Data", class = "m-4"), # m-4 increases margins to align with other inputs in that row
-    col_widths = c(3, 3, 3, 3)
+    selectInput(inputId = 'parameter', label = 'Parameter', choices = NULL)
   ),
-  layout_columns(
-    card(full_screen = TRUE, leafletOutput('map')),
-    card(full_screen = TRUE, plotlyOutput('plo'))
+  navset_card_underline(
+    nav_panel("Time Series Plot", plotlyOutput("tsPlot")),
+    nav_panel("Box Plot", p("Coming soon...")),
+    nav_panel("Tile Plot", p("Coming soon...")),
+    nav_panel("Map", leafletOutput("map"))
   )
+  # layout_columns(
+  # 
+  #   actionButton("show_dt", "Show Filtered Data", class = "m-4"), # m-4 increases margins to align with other inputs in that row
+  #   col_widths = c(3, 3, 3, 3)
+  # ),
 )
